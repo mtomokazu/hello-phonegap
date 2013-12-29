f6e5828e7f1a50b8bcf77dbc774d2cde63595b55
Matsushima Tomokazu <mtomokazu@gmail.com>
2013-12-29 22:45:10 +0900

    Import files created by 'phonegap local build android' command.

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/gen/com/phonegap/helloworld/R.java \
+ : /home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/res/drawable/icon.png \
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/res/values/strings.xml \
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/res/drawable-xhdpi/icon.png \
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/res/xml/config.xml \
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/res/drawable-hdpi/icon.png \
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/res/drawable-mdpi/icon.png \
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/res/drawable-ldpi/icon.png \
+/home/tomokazu/1210.PhoneGap/hello-phonegap/platforms/android/bin/AndroidManifest.xml \
